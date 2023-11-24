74dc29d316531346bb7daea9f7e7a4376050f19b
seongmun-hwang <hwang105814@gmail.com>
2023-11-25 00:30:57 +0900

    스즈미 맵에서 레이사 맵 이동 추가

--- Assets/Scripts/SuzumiMap/SuzumiInteract.cs
+++ Assets/Scripts/SuzumiMap/SuzumiInteract.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 //스즈미와 상호작용을 관리하는 코드
 public class SuzumiInteract : MonoBehaviour
@@ -9,22 +11,38 @@ public class SuzumiInteract : MonoBehaviour
     public GameObject OpenedSuzumi;
     public GameObject Koharu;
     public GameObject InteractBox;
+    public GameObject MoveMapBox;
     public float interactDistance; //코하루와 스즈미 거리
 
     public void Update()
     {
+        //스즈미랑 코하루 거리
         float distanceToSuzumi = Mathf.Abs(Koharu.transform.position.x - ClosedSuzumi.transform.position.x);
-        // F로 상호작용
-        Debug.Log(distanceToSuzumi);
+        //코하루랑 맵 이동 박스 거리
+        float distanceToMoveMap = Mathf.Abs(Koharu.transform.position.x - MoveMapBox.transform.position.x);
+        // 스즈미와 F로 상호작용
         if (distanceToSuzumi <= interactDistance) {
             InteractBox.SetActive(true);
             if (Input.GetKeyDown(KeyCode.F)){
                 Interact();
             }
-        } 
-        else if(distanceToSuzumi >= interactDistance)
+        }
+        else if (distanceToSuzumi >= interactDistance)
         {
             InteractBox.SetActive(false);
+        }
+        //맵 이동 상호 작용
+        if (distanceToMoveMap <= interactDistance)
+        {
+            MoveMapBox.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene("LeisaMap");
+            }
+        }
+        else if (distanceToMoveMap >= interactDistance)
+        {
+            MoveMapBox.SetActive(false);
         }
     }
     private void Interact()
--- Assets/Scripts/SuzumiMap/SuzumiMapManager.cs
+++ Assets/Scripts/SuzumiMap/SuzumiMapManager.cs
@@ -2,32 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.SceneManagement;
 
 public class SuzumiMapManager : MonoBehaviour
 {
-    public Tilemap tilemap; // 캐릭터가 이동할 타일맵 참조
-    // Start is called before the first frame update
+    public Tilemap tilemap;
+    private GameObject character;
     void Start()
     {
+        tilemap = FindObjectOfType<Tilemap>();
         //리소스 폴더에서 코하루 동적 로딩
         GameObject characterPrefab = Resources.Load<GameObject>("KoharuZzang");
-        GameObject character = Instantiate(characterPrefab);
-        //메인 카메라에 코하루 연결
-        //CameraFollowCharacter cameraScript = FindObjectOfType<CameraFollowCharacter>();
-        //cameraScript.target = character.transform;
+        character = Instantiate(characterPrefab);
         //스즈미 상호작용에 코하루 연결
         SuzumiInteract suzumiInteract = FindObjectOfType<SuzumiInteract>();
         suzumiInteract.Koharu = character;
-        //코하루 캐릭터에 타일맵 할당
-        //MoveKoharu moveKoharu = character.GetComponent<MoveKoharu>();
-        //moveKoharu.tilemap = tilemap;
-        //CameraFollowCharacter cameraFollowCharacter = character.GetComponent<CameraFollowCharacter>();
-        //cameraFollowCharacter.tilemap = tilemap;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
