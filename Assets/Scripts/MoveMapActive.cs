8761a77887482a7243fcebed24aad4df9d2facc2
seongmun-hwang <hwang105814@gmail.com>
2023-11-30 20:10:26 +0900

    맵 이동 오브젝트용 코드 분리

---
+++ Assets/Scripts/MoveMapActive.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MoveMapActive : MonoBehaviour
+{
+    public string sceneName;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
