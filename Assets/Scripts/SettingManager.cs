25892bdf6485d2e5f37f78add30d9f36a8953a96
seongmun-hwang <hwang105814@gmail.com>
2023-12-19 01:23:10 +0900

    해상도 설정 코드 추가

---
+++ Assets/Scripts/SettingManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SettingManager : MonoBehaviour
+{
+    public TMP_Dropdown resolutionDropdown;
+
+    void Start()
+    {
+        resolutionDropdown.onValueChanged.AddListener(ChangeResolution);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SetResolution_full()
+    {
+        Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, FullScreenMode.FullScreenWindow);
+    }
+    public void SetResolution_1920x1080()
+    {
+        Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+    }
+    public void SetResolution_1280x720()
+    {
+        Screen.SetResolution(1280, 720, FullScreenMode.Windowed);
+    }
+    public void ChangeResolution(int value)
+    {
+        switch (value)
+        {
+            case 0: // Full Screen
+                SetResolution_full();
+                Debug.Log(value);
+                break;
+            case 1: // 1920x1080
+                Debug.Log(value);
+                SetResolution_1920x1080();
+                break;
+            case 2: // 1280x720
+                Debug.Log(value);
+                SetResolution_1280x720();
+                break;
+            default:
+                Debug.LogError("Invalid resolution option.");
+                break;
+        }
+    }
+}
