3ea0c2e4a1a401d70bca31f57e8e320b2d7a0fb5
seongmun-hwang <hwang105814@gmail.com>
2024-01-01 16:31:54 +0900

    로딩 창 아리스 애니메이션 생성

--- Assets/Scripts/Loading/LoadingManager.cs
+++ Assets/Scripts/Loading/LoadingManager.cs
@@ -8,7 +8,7 @@ public class LoadingManager : MonoBehaviour
 {
     public TextMeshProUGUI Loading;
     public GameObject LoadingSpinner;
-    public float rotateSpeed = 100f;
+    private float rotateSpeed = 150.0f;
     void Update()
     {
         LoadingSpinner.transform.Rotate(Vector3.back, rotateSpeed * Time.deltaTime);
