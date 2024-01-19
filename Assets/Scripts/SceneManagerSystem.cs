39774ae136f1a1390d577390574f22a61918124c
seongmun-hwang <hwang105814@gmail.com>
2024-01-19 21:41:41 +0900

    SceneManagerSystem 싱글톤 생성

---
+++ Assets/Scripts/SceneManagerSystem.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerSystem : MonoBehaviour
+{
+    public static SceneManagerSystem Instance; // 싱글턴 인스턴스
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SaveCurrentSceneName();
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private void SaveCurrentSceneName()
+    {
+        // 현재 씬 이름을 저장
+        PlayerPrefs.SetString("PreviousScene", SceneManager.GetActiveScene().name);
+        PlayerPrefs.Save();
+    }
+}
