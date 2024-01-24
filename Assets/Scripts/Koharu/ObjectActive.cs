294480394577d24bc8b95089b8b4a88f4af59fca
seongmun-hwang <hwang105814@gmail.com>
2024-01-24 15:24:05 +0900

    interactBox 유효성 검사 추가

--- Assets/Scripts/Koharu/ObjectActive.cs
+++ Assets/Scripts/Koharu/ObjectActive.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ObjectActive : MonoBehaviour
 {
     public float activationDistance = 2.5f; // 활성화 거리
     public List<GameObject> interactBoxs; // 2D 스퀘어 오브젝트 리스트
     private Transform playerTransform; // 플레이어 위치
+    
     void Start()
     {
         interactBoxs = new List<GameObject>(GameObject.FindGameObjectsWithTag("InteractBox"));
@@ -16,8 +18,15 @@ public class ObjectActive : MonoBehaviour
     {
         foreach (GameObject square in interactBoxs)
         {
-            float distanceX = Mathf.Abs(playerTransform.position.x - square.transform.position.x);
-            square.SetActive(distanceX <= activationDistance);
+            if (square != null)
+            {
+                float distanceX = Mathf.Abs(playerTransform.position.x - square.transform.position.x);
+                bool isActive = distanceX <= activationDistance;
+                square.SetActive(isActive);
+
+                // 디버깅 로그
+                Debug.Log($"Square: {square.name}, Distance: {distanceX}, IsActive: {isActive}");
+            }
         }
     }
 }
